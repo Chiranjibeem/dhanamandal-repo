10441475090508cca796f0ed99eef1d8750d015d
Chiranjibee <43934590+Chiranjibeem@users.noreply.github.com>
2026-04-03 09:26:28 +0530

    Update DhanmandalApplication.java

--- src/main/java/com/dhanmandal/DhanmandalApplication.java
+++ src/main/java/com/dhanmandal/DhanmandalApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DhanmandalApplication {
 
     public static void main(String[] args) {
+        
         SpringApplication.run(DhanmandalApplication.class, args);
     }
 
